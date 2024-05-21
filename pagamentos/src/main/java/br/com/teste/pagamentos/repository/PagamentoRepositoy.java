7fc138df358af6bfcd95ea354db6d2c677f4c720
GISLENE NISI <nishi.gislene@gmail.com>
2024-05-21 19:55:03 -0300

    Add files via upload

---
+++ pagamentos/src/main/java/br/com/teste/pagamentos/repository/PagamentoRepositoy.java
@@ -0,0 +1,7 @@
+package br.com.teste.pagamentos.repository;
+
+import br.com.teste.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepositoy extends JpaRepository<Pagamento, Long> {
+}
